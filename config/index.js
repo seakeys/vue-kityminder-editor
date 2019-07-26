501b9deb2406372a5225d5ea0d8c669dcf3a3979
chenhaifei <649719596@qq.com>
2019-07-26 21:10:42 +0800

    123123

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '10.34.12.186', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 8088, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
